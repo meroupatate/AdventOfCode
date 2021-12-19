044fe0b59462bb20b7c898ff6dcf0bad41e9b156
Alice ZHEN <azhen@scaleway.com>
2021-12-19 22:42:25 +0100

    day 12 2021

--- 2021/helpers/utils.py
+++ 2021/helpers/utils.py
@@ -2,6 +2,8 @@ from enum import Enum
 
 class InputFile(Enum):
     EXAMPLE = "example_input.txt"
+    EXAMPLE2 = "example_input_2.txt"
+    EXAMPLE3 = "example_input_3.txt"
     REAL = "input.txt"
 
 
@@ -15,6 +17,18 @@ def extract_ints(day, input_file):
 
 def assert_example(f, expected):
     res = f(InputFile.EXAMPLE)
+    print(f"Example: {res}")
+    assert res == expected
+
+
+def assert_example2(f, expected):
+    res = f(InputFile.EXAMPLE2)
+    print(f"Example: {res}")
+    assert res == expected
+
+
+def assert_example3(f, expected):
+    res = f(InputFile.EXAMPLE3)
     print(f"Example: {res}")
     assert res == expected
 
